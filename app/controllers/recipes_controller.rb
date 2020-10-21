528dc7291ad14eece6cc2aef0dedca34d7f3316b
keisuke-sakuma <sakusuke0412@gmail.com>
2020-10-21 22:24:51 +0900

    料理投稿画面の実装

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -10,12 +10,18 @@ class RecipesController < ApplicationController
   end
 
   def create
-    @recipe = Recipe.new(id: params[:id])
+    @recipe = Recipe.new(recipe_params)
     if @recipe.save
       redirect_to @recipe
     end
   end
 
   def edit
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name,:description)
   end
 end
